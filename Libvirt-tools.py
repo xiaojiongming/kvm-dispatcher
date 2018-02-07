9d879bf8b749f03b07a2a1b8a0cd79cd6fbfaa06
minie <nieming92@foxmail.com>
2018-02-07 22:36:28 +0800

    tested fedora sanlock . next : why hang when qemu shutdown

--- Libvirt-tools.py
+++ Libvirt-tools.py
@@ -5,9 +5,9 @@ import sys
 
 
 class libvirttool:
-    def __init__(self):
+    def __init__(self, connectionuri="qemu:///system"):
         try:
-            self.__conntion = libvirt.open("qemu:///system")
+            self.__conntion = libvirt.open(connectionuri)
         except libvirt.libvirtError as e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             logging.ERROR("can not connect to local libvirt daemon" + repr(
@@ -17,4 +17,15 @@ class libvirttool:
         return self.__conntion
 
     def getvmlist(self):
-        self.__conntion.getAllDomainStats()
+        vmdict = {}
+        for vm in self.__conntion.listAllDomains():
+            vmdict[vm.ID()] = vm.name()
+        return vmdict
+
+    def getcomp(self):
+        return self.__conntion.getCapabilities()
+
+
+if __name__ == "__main__":
+    virtcontrole = libvirttool("qemu+tcp://minie@10.66.192.60:16510/system")
+    print(virtcontrole.getvmlist())
